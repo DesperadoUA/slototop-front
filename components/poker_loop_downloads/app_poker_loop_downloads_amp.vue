6e35a085f88af92489c201c923c06552f77eb88b
lazarev <lazarev-konstant@mail.ru>
2023-08-20 12:23:28 +0300

    Add poker loop download

--- components/poker_loop_downloads/app_poker_loop_downloads_amp.vue
+++ components/poker_loop_downloads/app_poker_loop_downloads_amp.vue
@@ -1,42 +1,51 @@
 <template>
-<div :class="'casinos '+bg">
+<amp-script layout="container" :src="`${config.BASE_URL[config.LANG]}/js/amp-poker.js`" class="sample">
+    <div :class="'casinos '+bg">
         <div class="slots">
             <div class="container">
                 <div class="slots__heading" v-if="title">
                     <h2 class="slots__ttl">{{title}}</h2>
                 </div>
                 <div class="slots__container items-wrap">
-                    <div    class="slot-item"
-                            v-for="(item, index) in currentPosts"
-                            :key="index">
-                        <div class="slot-item__logo">
-                            <NuxtLink no-prefetch :to="`${config.AMP_PREFIX}${item.permalink}`">
-                                <amp-img :src="item.thumbnail" alt="" width="214" height="136" />
-                            </NuxtLink>
-                        </div>
-                        <div class="slot-item__content">
-                            <div class="slot-item__name">{{item.title}}</div>
-                            <div class="slot-item__stats">
-                                <div class="star-rating">
-                                    <span class="star-rating__val" :style="item | widthRating"></span>
+                        <div    class="slot-item"
+                                v-for="(item, index) in currentPosts"
+                                :key="index">
+                            <div class="slot-item__logo">
+                                <NuxtLink no-prefetch :to="`${config.AMP_PREFIX}${item.permalink}`">
+                                    <amp-img :src="item.thumbnail" alt="" width="214" height="136" />
+                                </NuxtLink>
+                            </div>
+                            <div class="slot-item__content">
+                                <div class="slot-item__name">{{item.title}}</div>
+                                <div class="slot-item__stats">
+                                    <div class="star-rating">
+                                        <span class="star-rating__val" :style="item | widthRating"></span>
+                                    </div>
+                                    <strong class="slot-item__stats-val">{{item.rating}}/100</strong>
                                 </div>
-                                <strong class="slot-item__stats-val">{{item.rating}}/100</strong>
+                            </div>
+
+                            <div class="slot-item__btns">
+                                <a class="slot-item__btn --blue" :href="getRef(item)">{{translates.GO_TO[config.LANG]}}</a>
                             </div>
                         </div>
-
-                        <div class="slot-item__btns">
-                            <a class="slot-item__btn --blue" :href="getRef(item)">{{translates.GO_TO[config.LANG]}}</a>
-                        </div>
-                    </div>
                 </div>
-        </div>
-        <div class="items-more" v-if="value.length > (numberPostOnQuery*postCurrentPage)">
-            <button class="btn-secondary">
-                {{translates.SHOW_MORE[config.LANG]}}
-            </button>
+                <div class="loadContainer"></div>
+            </div>
+            <div class="items-more" v-if="value.length > (numberPostOnQuery*postCurrentPage)">
+                <button 
+                    :data-apiUrl="config.API_URL[config.LANG]" 
+                    :data-postsOnQuery="numberPostOnQuery"
+                    :data-ampPrefix="config.AMP_PREFIX"
+                    :data-post-type="post_type"
+                    :data-post-url="post_url"
+                    class="btn-secondary loadMoreBtn">
+                    {{translates.SHOW_MORE[config.LANG]}}
+                </button>
+            </div>
         </div>
     </div>
-</div>
+</amp-script>
 </template>
 
 <script>
@@ -56,6 +65,14 @@ import translateMixin from '~/mixins/translate'
             title: {
                 type: String,
                 default: undefined
+            },
+            post_type: {
+               default: 'page',
+               type: String
+            },
+            post_url: {
+                default: 'poker',
+                type: String
             }
         },
         mixins: [translateMixin],
